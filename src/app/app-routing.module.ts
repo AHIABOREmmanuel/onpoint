303383b3e41b8d51cd7fe08fda0bf7eedb401770
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-17 23:31:03 +0000

    17/04/2025

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { MainComponent } from './components/main/main.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PacksComponent } from './components/packs/packs.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:MainComponent},
+  {path: "about", component:AboutComponent},
+  {path: "service", component:ServicesComponent},
+  {path: "pack", component:PacksComponent},
+  {path: "portfolio", component:PortfolioComponent},
+  {path: "blog", component:BlogsComponent},
+  {path: "contact", component:ContactsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PacksComponent } from './components/packs/packs.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    NavComponent,
+    AboutComponent,
+    ServicesComponent,
+    PacksComponent,
+    PortfolioComponent,
+    BlogsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
