700a6ff7bfb87f8cc3ab35b174d1da8a146f2513
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-18 07:46:39 +0000

    18/04/2025

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,6 +30,7 @@ import { ContactsComponent } from './components/contacts/contacts.component';
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     provideClientHydration()
   ],
